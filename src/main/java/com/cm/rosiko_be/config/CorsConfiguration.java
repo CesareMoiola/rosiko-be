f97fd2efa38671dfa022635d3dafeac319bf4017
Cesare Moiola <cesaremoiola@gmail.com>
2022-04-10 23:26:42 +0200

    allowedOrigins

--- src/main/java/com/cm/rosiko_be/config/CorsConfiguration.java
+++ src/main/java/com/cm/rosiko_be/config/CorsConfiguration.java
@@ -14,7 +14,8 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
             }
         };
     }
