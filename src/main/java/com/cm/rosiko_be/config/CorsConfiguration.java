d452d48b7bc9dfe25c5dfc951fdab46ab3d62c3f
Cesare Moiola <cesaremoiola@gmail.com>
2022-04-10 23:03:17 +0200

    fix cors globale

--- src/main/java/com/cm/rosiko_be/config/CorsConfiguration.java
+++ src/main/java/com/cm/rosiko_be/config/CorsConfiguration.java
@@ -1,17 +1,21 @@
 package com.cm.rosiko_be.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer
-{
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("https://rosiko-fe.herokuapp.com/**")
-                .allowedMethods("GET", "POST");
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
