2cc48b43bdd167fcb39081bb355a60646e9a0d38
Cesare Moiola <cesaremoiola@gmail.com>
2022-04-13 15:53:46 +0200

    Endpoind di prod

--- src/main/java/com/cm/rosiko_be/config/EndPoint.java
+++ src/main/java/com/cm/rosiko_be/config/EndPoint.java
@@ -1,6 +1,6 @@
 package com.cm.rosiko_be.config;
 
 public class EndPoint {
-    //public static final String FRONTEND = "https://rosiko-fe.herokuapp.com";
-    public static final String FRONTEND = "http://localhost:3000";
+    public static final String FRONTEND = "https://rosiko-fe.herokuapp.com";
+    //public static final String FRONTEND = "http://localhost:3000";
 }
